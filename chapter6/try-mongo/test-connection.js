e91769c60e9c0844addf36f6dbb45b22605574b2
dachan <dachanisfull@gmail.com>
2024-10-15 22:37:58 +0900

    complete

--- chapter6/try-mongo/test-connection.js
+++ chapter6/try-mongo/test-connection.js
@@ -17,12 +17,16 @@ async function run() {
     await client.connect()
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 })
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    )
+    const adminDB = client.db("test").admin()
+    const listDatabases = await adminDB.listDatabases()
+    console.log(listDatabases)
+    return "OK"
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close()
   }
 }
-run().catch(console.dir)
+run()
+  .then(console.log)
+  .catch(console.dir)
+  .finally(() => client.close())
