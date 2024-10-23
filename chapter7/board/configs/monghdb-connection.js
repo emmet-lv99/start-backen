0f91aa02222906cbc2442ffbc9e84f4071eedf17
dachan <dachanisfull@gmail.com>
2024-10-23 23:20:24 +0900

    ~ing

--- chapter7/board/configs/monghdb-connection.js
+++ chapter7/board/configs/monghdb-connection.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb")
 
-const uri = "mongodb+srv://dachan:<db_password>@cluster0.pf9h8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/board"
+const uri =
+  "mongodb+srv://dachan:qkqhxod1!23@cluster0.pf9h8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/"
 
 module.exports = function (callback) {
   return MongoClient.connect(uri, callback)
