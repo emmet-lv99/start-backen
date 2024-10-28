eaa8dabc9d3152dd1d334860924419c894b0cb6c
dachan <dachanisfull@gmail.com>
2024-10-29 00:38:53 +0900

    ~ing

--- chapter7/board/app.js
+++ chapter7/board/app.js
@@ -3,6 +3,7 @@
 const express = require("express")
 const handlebars = require("express-handlebars")
 const app = express()
+const { ObjectId } = require("mongodb")
 
 const postService = require("./services/post-service")
 
@@ -31,7 +32,29 @@ app.get("/", async (req, res) => {
 })
 
 app.get("/write", (req, res) => {
-  res.render("write", { title: "테스트 게시판" })
+  res.render("write", { title: "테스트 게시판", mode: "create" })
+})
+
+app.get("/modify/:id", async (req, res) => {
+  const { id } = req.params.id
+  const post = await postService.getPostById(collection, req.params.id)
+  console.log(post)
+  res.render("write", { title: "테스트 게시판", mode: "modify", post })
+})
+
+app.post("/modify/", async (req, res) => {
+  const { id, title, writer, password, content } = req.body
+
+  const post = {
+    title,
+    writer,
+    password,
+    content,
+    createdDt: new Date().toISOString(),
+  }
+
+  const result = postService.updatePost(collection, id, post)
+  res.redirect(`/detail/${id}`)
 })
 
 app.post("/write", async (req, res) => {
@@ -43,6 +66,37 @@ app.post("/write", async (req, res) => {
 app.get("/detail/:id", async (req, res) => {
   const result = await postService.getDetailPost(collection, req.params.id)
   res.render("detail", { title: "테스트 게시판", post: result.value })
+})
+
+app.post("/check-password", async (req, res) => {
+  const { id, password } = req.body
+  const post = await postService.getPostByIdAndPassword(collection, {
+    id,
+    password,
+  })
+  if (!post) {
+    return res.status(404).json({ isExist: false })
+  } else {
+    return res.json({ isExist: true })
+  }
+})
+
+app.delete("/delete", async (req, res) => {
+  const { id, password } = req.body
+  try {
+    const result = await collection.deleteOne({
+      _id: ObjectId(id),
+      password: password,
+    })
+    if (result.deletedCount !== 1) {
+      console.log("삭제 실패")
+      return res.json({ isSuccess: false })
+    }
+    return res.json({ isSuccess: true })
+  } catch (error) {
+    console.error(error)
+    return res.json({ isSuccess: false })
+  }
 })
 
 app.listen(3000, async () => {
