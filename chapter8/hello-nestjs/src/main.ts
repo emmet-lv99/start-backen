7f6e12cc5c9c17ab4596b4ff750620373d46f75b
dachan <dachanisfull@gmail.com>
2024-11-21 00:45:49 +0900

    ~ing

--- chapter8/hello-nestjs/src/main.ts
+++ chapter8/hello-nestjs/src/main.ts
@@ -1,10 +1,8 @@
-import { NestFactory } from "@nestjs/core"
-import { HelloModule } from "./hello.module"
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-async function bootstrap () {
-  const app = await NestFactory.create(HelloModule)
-
-  await app.listen(3000, () => { console.log("server start!")})
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-bootstrap()
+bootstrap();
